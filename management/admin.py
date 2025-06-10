e66e4b88e7832527b79383bedb34a9b4b15bbb83
MR. Morningstar <ee8991471@gmail.com>
2025-06-10 22:44:15 +0600

    Admin_panel Added

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Emp
+
 # Register your models here.
+
+
+class EmpAdmin(admin.ModelAdmin):
+    list_display = ( 'name', 'phone','working')  
+    list_editable = ('phone','working')
+    search_fields=('name',)
+
+admin.site.register(Emp, EmpAdmin)
--- management/models.py
+++ management/models.py
@@ -9,3 +9,7 @@ class Emp(models.Model):
     address=models.CharField(max_length=150)
     working=models.BooleanField(default=True)
     department=models.CharField(max_length=10)
+
+
+    def __str__(self):
+        return self.name + " | " + self.email_id
